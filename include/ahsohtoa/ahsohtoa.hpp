2a99638754ce646a62a1b3a45e01fed971ebf375
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-11-03 14:46:16 +0100

    Apply clang-format

--- include/ahsohtoa/ahsohtoa.hpp
+++ include/ahsohtoa/ahsohtoa.hpp
@@ -1,77 +1,88 @@
 #pragma once
 
-#include <boost/pfr.hpp>
 #include <boost/mp11.hpp>
-#include <type_traits>
+#include <boost/pfr.hpp>
+
 #include <span>
+
+#include <type_traits>
 
 namespace ahso
 {
-template<typename T>
+template <typename T>
 using tuple_type_t = decltype(boost::pfr::structure_to_tuple(T{}));
 
-template<template<typename...> typename Container, typename T>
-struct arrays {
+template <template <typename...> typename Container, typename T>
+struct arrays
+{
   using tuple_type = tuple_type_t<T>;
   using vec_type = boost::mp11::mp_transform<Container, tuple_type>;
   using indices = std::make_index_sequence<boost::pfr::tuple_size_v<T>>;
   vec_type vec;
 
   //! Get the number of stored entities
-  std::size_t size() const noexcept {
-    return std::get<0>(vec).size();
-  }
+  std::size_t size() const noexcept { return std::get<0>(vec).size(); }
 
   //! Reserve storage for the entities (if Container supports it)
   void reserve(std::size_t space)
   {
-    [&] <std::size_t... N> (std::index_sequence<N...>) {
+    [&]<std::size_t... N>(std::index_sequence<N...>)
+    {
       (std::get<N>(vec).reserve(space), ...);
-    }(indices{});
+    }
+    (indices{});
   }
 
   //! Resize storage for the entities (if Container supports it)
   void resize(std::size_t space)
   {
-    [&] <std::size_t... N> (std::index_sequence<N...>) {
+    [&]<std::size_t... N>(std::index_sequence<N...>)
+    {
       (std::get<N>(vec).resize(space), ...);
-    }(indices{});
+    }
+    (indices{});
   }
 
   //! Create a new entity and return its index
   std::size_t create()
   {
-    [&] <std::size_t... N> (std::index_sequence<N...>) {
+    [&]<std::size_t... N>(std::index_sequence<N...>)
+    {
       (std::get<N>(vec).push_back({}), ...);
-    }(indices{});
+    }
+    (indices{});
     return size() - 1;
   }
 
   void erase(std::size_t index)
   {
-    [&] <std::size_t... N> (std::index_sequence<N...>) {
+    [&]<std::size_t... N>(std::index_sequence<N...>)
+    {
       (std::get<N>(vec).erase(std::get<N>(vec).begin() + index), ...);
-    }(indices{});
+    }
+    (indices{});
   }
 
   //! Add a new entity at the end of the storage.
   std::size_t add_entity(T e)
   {
-    [&] <std::size_t... N> (std::index_sequence<N...>) {
+    [&]<std::size_t... N>(std::index_sequence<N...>)
+    {
       (std::get<N>(vec).push_back(std::move(boost::pfr::get<N>(e))), ...);
-    }(indices{});
+    }
+    (indices{});
     return size() - 1;
   }
 
   //! Get a view on the list of components
-  template<typename Component>
+  template <typename Component>
   std::span<Component> components()
   {
     return std::get<Container<Component>>(vec);
   }
 
   //! Get a specific component from an entity
-  template<typename Component>
+  template <typename Component>
   auto& get(std::size_t index)
   {
     return std::get<Container<Component>>(vec)[index];
@@ -82,26 +93,27 @@ struct arrays {
   {
     // If our struct has a reference_type typedef, we construct it.
     // Note: ideally, metaclasses would allow to generate it.
-    if constexpr(requires { sizeof(typename T::reference_type); })
+    if constexpr (requires { sizeof(typename T::reference_type); })
     {
-      return [&] <std::size_t... N> (std::index_sequence<N...>) {
+      return [&]<std::size_t... N>(std::index_sequence<N...>)
+      {
         using ref_type = typename T::reference_type;
         return ref_type{std::get<N>(vec)[index]...};
-      }(indices{});
+      }
+      (indices{});
     }
     else
     {
       // Otherwise, we return a std::tuple<Component1&, Component2&, ...>
-      return [&] <std::size_t... N> (std::index_sequence<N...>) {
+      return [&]<std::size_t... N>(std::index_sequence<N...>)
+      {
         return std::tie(std::get<N>(vec)[index]...);
-      }(indices{});
+      }
+      (indices{});
     }
   }
 
   //! Get a reference to all the components of a given entity
-  auto operator[](std::size_t index) {
-    return get(index);
-  }
+  auto operator[](std::size_t index) { return get(index); }
 };
 }
-
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,62 +1,68 @@
 #include <ahsohtoa/ahsohtoa.hpp>
 
-struct vec3 { float x,y,z; };
-struct color { float r,g,b,a; };
-struct physics_component {
-    vec3 position;
-    vec3 speed;
-    vec3 acceleration;
+struct vec3
+{
+  float x, y, z;
+};
+struct color
+{
+  float r, g, b, a;
+};
+struct physics_component
+{
+  vec3 position;
+  vec3 speed;
+  vec3 acceleration;
 };
 
-struct render_component {
-    color col;
+struct render_component
+{
+  color col;
 };
 
-struct entity {
-    physics_component physics;
-    render_component render;
+struct entity
+{
+  physics_component physics;
+  render_component render;
 
-    // Not mandatory but it allows a better syntax when using get(index);
-    // Ideally metaclasses would be able to synthesize that from the original type.
-    struct reference_type {
-      physics_component& physics;
-      render_component& render;
-    };
+  // Not mandatory but it allows a better syntax when using get(index);
+  // Ideally metaclasses would be able to synthesize that from the original type.
+  struct reference_type
+  {
+    physics_component& physics;
+    render_component& render;
+  };
 };
 
 int main()
 {
-    ahso::arrays<std::vector, entity> e;
+  ahso::arrays<std::vector, entity> e;
 
-    e.reserve(500);
+  e.reserve(500);
 
-    e.add_entity({
-        .physics {
-            .position { 0., 0., 0. }
-          , .acceleration { 1., 0., 0. }
-        },
-        .render {
-            .col { 0., 1., 0., 1. }
-        }
-    });
+  e.add_entity(
+      {.physics{.position{0., 0., 0.}, .acceleration{1., 0., 0.}},
+       .render{.col{0., 1., 0., 1.}}});
 
-    e.get<physics_component>(0).position.x = 2;
+  e.get<physics_component>(0).position.x = 2;
 
-    // Print all the colors before the change
-    for(auto& [col] : e.components<render_component>()) {
-        printf("%f %f %f %f\n", col.r, col.g, col.b, col.a);
-    }
+  // Print all the colors before the change
+  for (auto& [col] : e.components<render_component>())
+  {
+    printf("%f %f %f %f\n", col.r, col.g, col.b, col.a);
+  }
 
-    e[0].render.col.r = 1.0;
+  e[0].render.col.r = 1.0;
 
-    // Print all the colors after the change
-    for(auto& [col] : e.components<render_component>()) {
-        printf("%f %f %f %f\n", col.r, col.g, col.b, col.a);
-    }
+  // Print all the colors after the change
+  for (auto& [col] : e.components<render_component>())
+  {
+    printf("%f %f %f %f\n", col.r, col.g, col.b, col.a);
+  }
 
-    assert(e.size() == 1);
-    e.erase(0);
-    assert(e.size() == 0);
+  assert(e.size() == 1);
+  e.erase(0);
+  assert(e.size() == 0);
 
-    e.resize(100);
+  e.resize(100);
 }
